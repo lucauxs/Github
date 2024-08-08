7afc84283a3a999c54bb718f866b485d792b32dc
Lucas Coco <lucasgcocos@gmail.com>
2024-08-08 11:47:34 -0300

    Resolução de problemas

---
+++ teste2.py
@@ -0,0 +1,21 @@
+print("Este é um teste de adição de um outro código no mesmo repositório.")
+
+escolha = input("Digite se aceita continuar com o programa: (Y/N): ")
+
+if escolha != "Y":
+    print("Foi um prazer apresentar o programa, até mais!")
+
+else:
+    print("Vamos dar continuidade ao programa")
+    
+    print("\nDigite o seu estilo de amendoin preferido:")
+
+    tipo = input("\namem / torrado? ")
+
+    if tipo.upper =="torrado":
+        print("escolha melhor! HEREGE!!")
+
+    elif tipo.upper =="amem" or "amen":
+        print("God bless you!")
+    
+
